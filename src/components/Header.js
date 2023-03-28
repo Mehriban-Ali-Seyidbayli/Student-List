99be3daf044d1559ef7c19939bac208488a15a63
Mehriban-Ali-Seyidbayli <mehriban.ali.seyidbayli.94@gmail.com>
2023-03-28 17:38:30 +0400

    gif and description

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom"
 
 const Header = () => {
     return (
-        <nav className="navbar navbar-expand-sm navbar-light bg-light">
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">Navbar</Link>
+                <Link className="navbar-brand" to="/">Students</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
